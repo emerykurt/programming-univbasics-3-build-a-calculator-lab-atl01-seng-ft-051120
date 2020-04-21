5ba6e656eb8136245b26cc9aefef56c81a457553
Emery K Lumsden II <emerylumsden@gmail.com>
2020-04-21 12:12:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,31 @@
 def addition(num1, num2)
-
+  num1 = 5
+  num2 = 4
+  sum = #{num2} + #{num1}
+  sum 
 end
+
 
 def subtraction(num1, num2)
-
-end
-
-def division(num1, num2)
-
+  subtraction = #{num2} - #{num1}
+  subtraction
 end
 
 def multiplication(num1, num2)
+  multiplication = #{num1} * #{num2}
+  multiplication
+end
 
+def division(num1, num2)
+  division = #{num2} / #{num1}
+  division
 end
 
 def modulo(num1, num2)
-
+  modulo = #{num2} % #{num1}
+  modulo
 end
-
+  
 def square_root(num)
 
 end
